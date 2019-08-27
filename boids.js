bb7692146d25d99606fa2a31c433c97662826f38
Valentin Voigt <mail@valentinvoigt.info>
2019-08-27 18:48:16 +0200

    Add obstacle avoidance

--- boids.js
+++ boids.js
@@ -1,6 +1,39 @@
-const range = 200;
-const spacing = 25;
+const boidRange = 200;
+const boidSpacing = 25;
+const obstacleSpacing = 50;
+
 var boids = [];
+var obstacles = [];
+
+class CircleObstacle {
+
+	constructor(x, y, radius) {
+		this.pos = createVector(x, y);
+		this.radius = radius;
+	}
+
+	draw() {
+		push();
+
+		translate(this.pos.x, this.pos.y);
+		stroke(255, 255, 255);
+		noFill();
+		circle(0, 0, this.radius);
+
+		pop();
+	}
+
+	collide(pos, dir) {
+		if (this.pos.dist(pos) < this.radius + obstacleSpacing) {
+			let force = p5.Vector.sub(pos, this.pos);
+			force.div(this.pos.dist(pos));
+			return force;
+		} else {
+			return createVector(0, 0);
+		}
+	}
+
+}
 
 class Boid {
 
@@ -17,7 +50,7 @@ class Boid {
 			if (boid === this)
 				continue;
 
-			if (boid.pos.dist(this.pos) < range) {
+			if (boid.pos.dist(this.pos) < boidRange) {
 				neighbours.push(boid);
 			}
 		}
@@ -25,16 +58,29 @@ class Boid {
 		return neighbours;
 	}
 
+	getObstaclesForce() {
+		let force = createVector(0, 0);
+
+		if (obstacles.length > 0) {
+			for (let obstacle of obstacles) {
+				force.add(obstacle.collide(this.pos, this.dir));
+			}
+			force.normalize();
+		}
+
+		return force;
+	}
+
 	getBoundsForce() {
 		let antiBounds = createVector(0, 0);
 
-		if (this.pos.x < range)
+		if (this.pos.x < boidRange)
 			antiBounds.add(createVector(1, 0));
-		if (this.pos.y < range)
+		if (this.pos.y < boidRange)
 			antiBounds.add(createVector(0, 1));
-		if (this.pos.x > width-range)
+		if (this.pos.x > width-boidRange)
 			antiBounds.add(createVector(-1, 0));
-		if (this.pos.y > height-range)
+		if (this.pos.y > height-boidRange)
 			antiBounds.add(createVector(0, -1));
 
 		return antiBounds;
@@ -71,7 +117,7 @@ class Boid {
 	}
 
 	getSeparationForce(neighbours) {
-		let nearNeighbours = neighbours.filter((b) => this.pos.dist(b.pos) < spacing);
+		let nearNeighbours = neighbours.filter((b) => this.pos.dist(b.pos) < boidSpacing);
 
 		if (nearNeighbours) {
 			let separation = createVector(0, 0);
@@ -98,6 +144,7 @@ class Boid {
 		this.dir.add(p5.Vector.mult(this.getSeparationForce(neighbours), 0.1));
 		this.dir.limit(2);
 		this.dir.add(p5.Vector.mult(this.getBoundsForce(), 0.05));
+		this.dir.add(p5.Vector.mult(this.getObstaclesForce(), 0.05));
 		this.pos.add(p5.Vector.mult(this.dir, this.speed * deltaTime));
 	}
 
@@ -118,6 +165,8 @@ class Boid {
 function setup() {
 	createCanvas(windowWidth, windowHeight);
 
+	obstacles.push(new CircleObstacle(width/2, height/2, 200));
+
 	for (let i = 0; i < 100; i++) {
 		boids.push(new Boid(random(width), random(height), random(TWO_PI)));
 	}
@@ -126,11 +175,11 @@ function setup() {
 function draw() {
 	background(42);
 
-	for (let boid of boids) {
+	for (let boid of boids)
 		boid.move();
-	}
 
-	for (let boid of boids) {
+	for (let obstacle of obstacles)
+		obstacle.draw();
+	for (let boid of boids)
 		boid.draw();
-	}
 }
